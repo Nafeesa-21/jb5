fa0f68f07d842723ece84a7f0c4ee559590f4496
Gingirikani <167854825+Gingirikani@users.noreply.github.com>
2024-07-03 05:11:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,9 @@ import streamlit as st
 
 """
 # news prediction model
+analysing news articles
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.prediction with ml models
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
