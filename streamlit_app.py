337d812da10750d6e3611698dbdbc7ea1ca96c39
Gingirikani <167854825+Gingirikani@users.noreply.github.com>
2024-07-03 05:48:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,16 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+#----------sidebar
+page = st.sidebar.selectbox('page navigator',["predictor","model analysis"])
+
 """
 # news prediction model
 analysing news articles
 
 st.prediction with ml model
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
