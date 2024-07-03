84fc0ef91fd57025d79ad38f57d36571c9513535
Gingirikani <167854825+Gingirikani@users.noreply.github.com>
2024-07-03 06:43:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,4 @@ if page == "predictor":
   st.markdown("analysing news articles")
   columns = st.columns([2,1])
   
-  # model input
-  model_input = column[0].expander(label="prediction with ML models")
-  
   
