db9b4afd9c19822836182a4c3ec6833a08ea47d2
Gingirikani <167854825+Gingirikani@users.noreply.github.com>
2024-07-03 06:22:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,6 @@ page = st.sidebar.selectbox('page navigator', ["predictor", "model analyis"])
 if page == "predictor":
   #-----------inputs
   st.markdown("analysing news articles")
+  columns = st.columns([2,1])
+
   
